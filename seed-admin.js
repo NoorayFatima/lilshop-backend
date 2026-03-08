f6dacc75d55de37078ba391bb102ef248ef0a5bf
Nooray Fatima <nf569100@gmail.com>
2026-03-08 10:36:41 -0700

    feat: creating admin

---
+++ seed-admin.js
@@ -0,0 +1,14 @@
+const { exec } = require('child_process');
+// Change these to what the client wants
+const email = "admin@lilshop.com"; 
+const password = "Password123!"; 
+
+console.log("Checking/Creating Admin User...");
+exec(`npx medusa user -e ${email} -p ${password}`, (error, stdout, stderr) => {
+    if (error) {
+        // If user exists, it will error; we can ignore that
+        console.log("Admin might already exist or error occurred.");
+    } else {
+        console.log("Admin created successfully!");
+    }
+});
