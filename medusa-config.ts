3f4bfc3225380483faa4ddbdd10ab33bec17760f
Nooray Fatima <nf569100@gmail.com>
2026-03-16 09:02:02 -0700

    fix: ! missing

--- medusa-config.ts
+++ medusa-config.ts
@@ -30,9 +30,9 @@ export default defineConfig({
     {
       resolve: "medusa-file-cloudinary",
       options: {
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET,
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+        api_key: process.env.CLOUDINARY_API_KEY!,
+        api_secret: process.env.CLOUDINARY_API_SECRET!,
       },
     },
   ],
